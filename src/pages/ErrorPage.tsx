b2050f41b5f808e1c8a9369911beabe660067c03
c1DeepInside <vanya0759@gmail.com>
2023-03-13 21:39:19 +0300

    feat: add link to main page in error page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 
 export default class About extends Component {
   render() {
-    return <div>Error</div>;
+    return (
+      <div>
+        <h2>Error 404</h2>
+        <NavLink to={'/'}>Click to go to the main page</NavLink>
+      </div>
+    );
   }
 }
