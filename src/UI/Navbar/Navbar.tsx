2292e11eef943f3c3ea5f9347da8c218f0101f41
c1DeepInside <vanya0759@gmail.com>
2023-03-14 20:50:46 +0300

    feat: add header with page name

--- src/UI/Navbar/Navbar.tsx
+++ src/UI/Navbar/Navbar.tsx
@@ -1,30 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import cl from './Navbar.module.scss';
 
-const Navbar = () => {
-  return (
-    <div className={cl.navbar}>
-      <nav className={cl.nav}>
-        <NavLink
-          className={({ isActive }) =>
-            isActive ? `${cl.nav__btn} ${cl.active__btn}` : `${cl.nav__btn}`
-          }
-          to="/"
-        >
-          Cards
-        </NavLink>
-        <NavLink
-          className={({ isActive }) =>
-            isActive ? `${cl.nav__btn} ${cl.active__btn}` : `${cl.nav__btn}`
-          }
-          to="/about"
-        >
-          About us
-        </NavLink>
-      </nav>
-    </div>
-  );
-};
+interface Props {
+  text?: string;
+}
 
-export default Navbar;
+export default class Navbar extends Component<Props, { currentPage: string }> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      currentPage: '',
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ currentPage: window.location.pathname === '/' ? 'Cards' : 'About us' });
+  }
+
+  changeCurrentPage = (value: string) => {
+    this.setState({ currentPage: value });
+  };
+
+  render() {
+    return (
+      <div className={cl.navbar}>
+        <nav className={cl.nav}>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? `${cl.nav__btn} ${cl.active__btn}` : `${cl.nav__btn}`
+            }
+            to="/"
+            onClick={() => this.changeCurrentPage('Cards')}
+          >
+            Cards
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? `${cl.nav__btn} ${cl.active__btn}` : `${cl.nav__btn}`
+            }
+            to="/about"
+            onClick={() => this.changeCurrentPage('About us')}
+          >
+            About us
+          </NavLink>
+          <p className={cl.current__page}>{this.state.currentPage}</p>
+        </nav>
+      </div>
+    );
+  }
+}
